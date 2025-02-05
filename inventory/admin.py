76e7661968f62cdd144ca5a912f24b83f17b919d
Mark Louil Diacamos <dmarklouil@gmail.com>
2025-02-05 22:48:47 +0800

    Added models to the database

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Axie, Transaction
 
 # Register your models here.
+
+class AxieAdmin(admin.ModelAdmin):
+    list_display = ('axie_id', 'axie_class', 'breed_count', 'cards', 'purchase_price', 
+                    'purchase_date', 'status') 
+    
+    ordering = ('axie_id', )
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('axie', 'transaction_type', 'amount', 'date')
+    
+    ordering = ('date', )
+
+admin.site.register(Axie, AxieAdmin)
+admin.site.register(Transaction, TransactionAdmin)
