1cc87d7a6d3026ffa9f8ef9751cbbdf0f339d1cb
Mark Louil Diacamos <dmarklouil@gmail.com>
2025-02-08 22:16:37 +0800

    Made axie edit feature work

--- inventory/urls.py
+++ inventory/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('dashboard/', views.dashboard_view, name='dashboard_view'),
     path('axies/', views.axie_list_view, name='axies_view'),
     path('axies/add-axie/', views.add_axie_form_view, name='add_axie_view'),
+    path('axies/edit-axie/<int:axie_id>/', views.edit_axie_form_view, name='edit_axie_form_view'),
     path('transactions/', views.transaction_list_view, name='transaction_list_view'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import Axie, Transaction 
 from django.contrib import messages
 
@@ -36,6 +36,34 @@ def add_axie_form_view(request):
 
         # Stop here for now
     return render(request, 'axie-form.html')
+
+def edit_axie_form_view(request, axie_id):
+    try:
+        axie = Axie.objects.get(axie_id=axie_id)
+        if request.method == 'POST':
+            fields = ['breedCount', 'purchasePrice', 'purchaseDate', 'sellingPrice', 'dateSold', 'status']
+            data = {field: request.POST.get(field) for field in fields}
+
+            axie_data = {
+                'breed_count': data['breedCount'],
+                'purchase_price': data['purchasePrice'],
+                'purchase_date': data['purchaseDate'],
+                'selling_price': data['sellingPrice'],
+                'sell_date': data['dateSold'],
+                'status': data['status']
+            }
+
+            Axie.objects.filter(axie_id=axie_id).update(**axie_data)
+
+            messages.success(request, 'Axie updated successfully.')
+        
+        context = {
+            'axie': axie 
+        }
+
+        return render(request, 'axie-form.html', context)
+    except Axie.DoesNotExist:
+        return HttpResponse('Axie not found.')
 
 def transaction_list_view(request):
     transactions = Transaction.objects.all().order_by('date')
